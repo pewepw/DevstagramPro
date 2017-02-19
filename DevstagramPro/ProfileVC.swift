40a72782764b91999c4fcad0be71bafb5e9e3d67
Harry Ng <poripori1030@gmail.com>
2017-02-19 20:05:52 +0800

    revert back to like function

--- DevstagramPro/ProfileVC.swift
+++ DevstagramPro/ProfileVC.swift
@@ -9,51 +9,18 @@
 import UIKit
 
 class ProfileVC: UIViewController {
-    
-    var user: User!
 
-    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        collectionView.dataSource = self
-        fetchUser()
+        
         
     }
 
-    func fetchUser() {
-        API.User.observeCurrentUser { (user) in
-            self.user = user
-            self.collectionView.reloadData()
-        }
-    }
     
     
     
     
     
-    
-}
-
-
-extension ProfileVC: UICollectionViewDataSource {
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath)
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        let reusableCell = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionElementKindSectionHeader , withReuseIdentifier: "ProfileVCReusable", for: indexPath) as! ProfileVCReusable
-        if let user = self.user {
-            reusableCell.user = user
-        }
-        
-        return reusableCell
-    }
     
 }
--- DevstagramPro/UserAPI.swift
+++ DevstagramPro/UserAPI.swift
@@ -8,39 +8,18 @@
 
 import Foundation
 import FirebaseDatabase
-import FirebaseAuth
+
 class UserAPI {
     var REF_USERS = FIRDatabase.database().reference().child("users")
     
     func observeUser(withId uid: String, completion: @escaping (User) -> Void) {
-        REF_USERS.child(uid).observeSingleEvent(of: .value, with: {
-            snapshot in
+        REF_USERS.child(uid).observeSingleEvent(of: .value, with: { (snapshot) in
             if let dict = snapshot.value as? [String: Any] {
                 let user = User.transformUser(dict: dict)
                 completion(user)
             }
         })
-    }
-    
-    func observeCurrentUser(completion: @escaping (User) -> Void) {
-        guard let currentUser = FIRAuth.auth()?.currentUser else {
-            return
-        }
-        REF_USERS.child(currentUser.uid).observeSingleEvent(of: .value, with: {
-            snapshot in
-            if let dict = snapshot.value as? [String: Any] {
-                let user = User.transformUser(dict: dict)
-                completion(user)
-            }
-        })
-    }
-    
-    var REF_CURRENT_USER: FIRDatabaseReference? {
-        guard let currentUser = FIRAuth.auth()?.currentUser else {
-            return nil
-        }
         
-        return REF_USERS.child(currentUser.uid)
+        
     }
 }
-
